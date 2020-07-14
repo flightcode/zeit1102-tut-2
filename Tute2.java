8932d8c467da2770e4c00b743d464df1fd38aae9
Elliot Parker <elliotccp@protonmail.com>
2020-07-15 09:41:11 +1000

    Add initial content

---
+++ Tute2.java
@@ -0,0 +1,22 @@
+/**
+ * @Elliot Parker
+ * @2020-07-15
+ */
+
+import java.util.*;
+
+public class Tute2
+{
+    Scanner scan = new Scanner(System.in);
+    
+    public void init()
+    {
+        
+    }
+    
+    public static void main(String [] args)
+    {
+        Tute2 t2 = new Tute2();
+        t2.init();
+    }
+}
