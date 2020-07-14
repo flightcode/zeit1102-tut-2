32821b7d7e52b89532d7ec5d47793a1cf7f91732
Elliot Parker <elliotccp@protonmail.com>
2020-07-15 09:51:10 +1000

    Add task 1 content

--- Tute2.java
+++ Tute2.java
@@ -19,4 +19,24 @@ public class Tute2
         Tute2 t2 = new Tute2();
         t2.init();
     }
+    
+    int random(int i1, int i2)
+    {
+        Random r = new Random();
+        int low;
+        int high;
+        if(i1 > i2)
+        {
+            high = i1;
+            low = i2;
+        } else if(i2 > i1)
+        {
+            high = i2;
+            low = i1;
+        } else
+        {
+            return 0;
+        }
+        return r.nextInt(high - low) + low;
+    }
 }
